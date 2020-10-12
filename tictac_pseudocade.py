794607ab465b5077ca0c254feef2e2aa7c7ba717
Bjordammen <Will_Bjordammen1@baylor.edu>
2020-10-12 16:27:53 -0500

    tic tac check in

---
+++ tictac_pseudocade.py
@@ -0,0 +1,29 @@
+# Create an empty board. List within a list.
+# Print board to screen.
+# Initialize a boolean variable. repeat = true
+# while repeat = true
+#   ask user for row,col
+#   place it on board
+#   print the board
+#   get x's by row and cols
+#   if any element in array = 3:
+#       then user wins!
+#       repeat = False
+#   get computer row, col = r, c
+#   while board[r,c] == 'X' or 'O':
+#       get row, col again
+#   put it on board
+#   print board
+#   get row count and col count for computer
+#   if any element == 3
+#       then computer wins
+#       repeat = False
+
+#   count_row = np.count_nonzero(myarrary=='X')
+#   print(count_row)
+
+#   count_row = np.count_nonzero(myarray=='X', axis=1)
+#   print(count_row)
+
+#   count_col = np.count_nonzero(myarray=='X', axis=0)
+#   print(count_col)
